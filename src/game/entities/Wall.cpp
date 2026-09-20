8426cd79cfb624cdef4cf057b973a7f16ea84cc8
Wings <kelaskarmanoj18@gmail.com>
2026-09-20 10:14:27 +0530

    This is a Wall which contains Wall logic and functions and which is made from Body(template class)

---
+++ src/game/entities/Wall.cpp
@@ -0,0 +1,58 @@
+#include "game/entities/Wall.h"
+
+namespace sg {
+
+    Wall::Wall(int head, int tail)
+        : m_wallBody(head, tail) {}
+
+    void Wall::init(char sym) {
+        m_wallBody.init(sym);
+    }
+
+    using uint = std::size_t;
+
+    void Wall::setTheWall() {
+        int i = 0;
+        setRight(i, 0, 30);
+        setLeft(i, 50, 30);
+        setTop(i, 50, 0);
+        setDown(i, 50, 30);
+    }
+
+    void Wall::setRight(int& i, int xLimit, int yLimit) {
+        for(uint lim = 0; lim < yLimit; lim++) {
+            m_wallBody.setCoord(i++, xLimit, lim);
+        }
+    }
+
+    void Wall::setLeft(int& i, int xLimit, int yLimit) {
+        for(uint lim = 0; lim < yLimit; lim++) {
+            m_wallBody.setCoord(i++, xLimit - 1, lim);
+        }
+    }
+
+    void Wall::setTop(int& i, int xLimit, int yLimit) {
+        for(uint lim = 0; lim < xLimit; lim++) {
+            m_wallBody.setCoord(i++, lim, yLimit);
+        }
+    }
+
+    void Wall::setDown(int& i, int xLimit, int yLimit) {
+        for(uint lim = 0; lim < xLimit; lim++) {
+            m_wallBody.setCoord(i++, lim, yLimit - 1);
+        }
+    }
+
+    const Cell& Wall::getCell(int index) const noexcept {
+        return m_wallBody.getBody()[index];
+    }
+
+    int Wall::head() const noexcept {
+        return m_wallBody.getHead();
+    }
+
+    int Wall::tail() const noexcept {
+        return m_wallBody.getTail();
+    }
+
+}
