909cda4ab1a9fc8bf622169e960246a548e76014
Wings <kelaskarmanoj18@gmail.com>
2026-09-20 10:53:30 +0530

    * Changed file, folder and class names: - Folder name render-system --> render - File name GameRender --> Window - class name GameRender --> Window * Also wrapped Window under render namespace

---
+++ include/engine/render/Window.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#include "game/entities/Snake.h"
+#include "game/entities/Wall.h"
+
+
+namespace render {
+        
+    class Window {
+    private:
+        std::array<std::array<char, 50>, 30> m_frame;
+
+    public:
+        Window();
+
+        void frameReset();
+
+        template <typename T>
+        void draw(const T& cell);
+
+        void display() const noexcept;
+    };
+
+    template <typename T>
+    void Window::draw(const T& cell) {
+        int i;
+        for(i = cell.tail(); i < cell.head(); i++) {
+            m_frame[cell.getCell(i).getY()][cell.getCell(i).getX()] = cell.getCell(i).m_symbol;
+        }
+    }
+
+}
