9a95c8e481b454a01bcc14d32726b1c925517d63
Wings <kelaskarmanoj18@gmail.com>
2026-09-20 10:09:43 +0530

    This is like a atom which is used to make different structures

---
+++ src/engine/core/Cell.cpp
@@ -0,0 +1,16 @@
+#include "engine/core/Cell.h"
+
+namespace sg {
+
+    Cell::Cell(char sym)
+        : m_symbol(sym) {}
+
+    int Cell::getX() const noexcept {return m_coord.x;}
+
+    int Cell::getY() const noexcept {return m_coord.y;}
+
+    void Cell::setX(int x) noexcept {m_coord.x = x;}
+
+    void Cell::setY(int y) noexcept {m_coord.y = y;}
+
+}
