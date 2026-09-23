0de348d73762c823000a9e0850f53e819d7d5dd1
Wings <kelaskarmanoj18@gmail.com>
2026-09-23 23:54:16 +0530

    Changed the names of variables

--- include/engine/render/Window.h
+++ include/engine/render/Window.h
@@ -15,25 +15,26 @@ namespace Engine::Render {
 
         template <typename T>
         void draw(const T& cell);
-
+        
         void display() const noexcept;
     };
-
+    
     template <typename T>
     void Window::draw(const T& cell) {
         if(cell.tail() < cell.head()) {
             for(int i = cell.tail(); i <= cell.head(); i++) {
-                m_frame[cell.getCell(i).getY()][cell.getCell(i).getX()] = cell.getCell(i).m_symbol;
+                m_frame[cell.getCell(i).m_transform.y][cell.getCell(i).m_transform.x] = cell.getCell(i).m_sprite;
             }
-        }else {
+        } else {
             for(int i = 0; i <= cell.head(); i++) {
-                m_frame[cell.getCell(i).getY()][cell.getCell(i).getX()] = cell.getCell(i).m_symbol;
+                m_frame[cell.getCell(i).m_transform.y][cell.getCell(i).m_transform.x] = cell.getCell(i).m_sprite;
             }
             
             for(int i = cell.tail(); i < cell.fullSize(); i++) {
-                m_frame[cell.getCell(i).getY()][cell.getCell(i).getX()] = cell.getCell(i).m_symbol;
+                m_frame[cell.getCell(i).m_transform.y][cell.getCell(i).m_transform.x] = cell.getCell(i).m_sprite;
             }
         }
     }
+
 
 }
