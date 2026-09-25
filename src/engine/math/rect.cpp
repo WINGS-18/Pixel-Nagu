a3b3b7765aa6edaf8c78257e549162502bf1b38c
Wings <kelaskarmanoj18@gmail.com>
2026-09-25 21:27:29 +0530

    Reworked the Rect struct such that now it holds top left(m_min) and top right (m_max) only 2 corners and yet we can detect proper overlapping and collisions

--- src/engine/math/rect.cpp
+++ src/engine/math/rect.cpp
@@ -2,7 +2,19 @@
 
 namespace Engine::Math {
 
-    Rect::Rect(Vector4 upper, Vector4 lower)
-        : m_upper(upper), m_lower(lower) {}
+    Rect::Rect(Vector2C min, Vector2C max)
+        : m_min(min), m_max(max) {}
 
+    bool Rect::intersects(const Rect& other) {
+        if (
+            m_max.x < other.m_min.x || // A's right edge is to the left of B's left edge
+            m_min.x > other.m_max.x || // A's left edge is to the right of B's right edge
+            m_max.y < other.m_min.y || // A's bottom edge is above B's top edge
+            m_min.y > other.m_max.y    // A's top edge is below B's bottom edge
+        ) {
+            return false; // No overlap
+        }
+        
+        return true; // Overlap!
+    }
 }
