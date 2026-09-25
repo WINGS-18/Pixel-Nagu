9d4ead2510767309c4a5c0129715cbe7b9cde43e
Wings <kelaskarmanoj18@gmail.com>
2026-09-25 21:29:17 +0530

    Changed members names from [m_vec1 -> m_vecLeft && m_vec2 -> m_vecRight

--- include/engine/math/vector4.h
+++ include/engine/math/vector4.h
@@ -5,12 +5,13 @@
 namespace Engine::Math {
 
     struct Vector4 {
-        Vector2C m_vec1 {0, 0};
-        Vector2C m_vec2 {0, 0};
+        Vector2C m_vecLeft {0, 0};
+        Vector2C m_vecRight {0, 0};
 
         Vector4() = default;
 
-        Vector4(Vector2C vec1, Vector2C vec2);    
+        Vector4(Vector2C vec1, Vector2C vec2);
+
     };
     
 }
